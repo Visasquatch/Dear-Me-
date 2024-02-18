a85448def9f966bdc635552a8e7f5b8e3886a887
Vincent Asagba <95535905+Visasquatch@users.noreply.github.com>
2024-02-18 10:08:03 +0000

    Add files via upload

---
+++ Dear Me,/script.js
@@ -0,0 +1,30 @@
+let locked = true;
+
+function unlockDiary() {
+  const password = document.getElementById('password').value;
+ if (password === '1234') {
+    document.getElementById('lock').style.display = 'none';
+    document.getElementById('diary').style.display = 'block';
+    locked = false;
+  } else {
+    alert('Incorrect password!');
+    event.preventDefault();
+    return;
+    locked = true;
+  } 
+}
+function lockDiary() {
+  document.getElementById('lock').style.display = 'block';
+  document.getElementById('diary').style.display = 'none';
+  document.getElementById('password').value = '';
+  locked = true;
+}
+
+function saveEntry(locked) {
+  if (locked) {
+    alert('Please unlock the diary first.');
+    return;
+  }
+  const entryHead = document.getElementById('entryHead').value;
+  alert('Entry saved\nTitle: ' + entryHead);
+}
